ae08759bc4d284afd8bff31f9514f00eb2ceec5c
KhaHuflitK29 <nguyendamkha@gmail.com>
2024-07-21 16:27:27 +0700

    remove unnecessary files

--- InfixToPostfixNew.cpp
+++ InfixToPostfixNew.cpp
@@ -1,6 +1,3 @@
-﻿// InfixToPostfixCpp.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
 #include<string>
 #include<stack>
@@ -21,10 +18,10 @@ bool IsOperator(char token) {
     default:
         return false;
     }
-    
+
 }
 
-int GetPrecedence(char token) { 
+int GetPrecedence(char token) {
     if (token == '^')
         return 3;
     else if (token == '*' || token == '/')
@@ -32,7 +29,7 @@ int GetPrecedence(char token) {
     else if (token = '+' || token == '-')
         return 1;
     else
-        return -1; 
+        return -1;
 }
 
 char Associativity(char token) {
@@ -40,29 +37,30 @@ char Associativity(char token) {
         return 'r';
 }
 
-string ValidString(string postfix) {
-    string res = "";
-    for (int i = 0; i < postfix.length() - 1; i++) {
-        res += postfix[i];
-    }
-    return res;
-}
+
 
 
 //Shunting-yard Algorithm
 string ConvertInfixToPostfix(string infix) {
     stack<char> op;
-    string output;
+    string output = "";
     for (int i = 0; i < infix.length();i++) {
-        string element = "";
+        //check if the scanned token is operand?
         if (!IsOperator(infix[i])) {
+
+            //to handle cases where operands have more than 1 digit
             while (!IsOperator(infix[i]) && i < infix.length()) {
-                element += infix[i];
+                output += infix[i];
                 i++;
             }
+            //Make sure there is a space after each element (both operand and operator).
+            if (IsOperator(infix[i]) || i == infix.length())
+                output += ' ';
+            --i;//Make sure the for loop goes through the entire element
         }
-        output += element+' ';
-        if (op.empty() || infix[i] == '('||op.top()=='(')//luc nay scanned token da la operator
+
+        //Just need to handle cases where it is an operator
+        else if (op.empty() || infix[i] == '(' || op.top() == '(')
         {
             op.push(infix[i]);
         }
@@ -80,8 +78,11 @@ string ConvertInfixToPostfix(string infix) {
             op.push(infix[i]);
         }
         else if (GetPrecedence(infix[i]) <= GetPrecedence(op.top())) {
+            //If the top of the stack and the scanned operator have equal precedence,
+            //  then if the scanned operator is right-associative, it has higher precedence than
+            //  the top of the stack.
             if (Associativity(infix[i]) == 'r')
-                goto Perform1;
+                goto Perform1; 
             while (!op.empty() && op.top() != '(' && GetPrecedence(op.top()) >= GetPrecedence(infix[i])) {
                 output += op.top();
                 output += ' ';
@@ -94,33 +95,26 @@ string ConvertInfixToPostfix(string infix) {
     }
     while (!op.empty()) {
         output += op.top();
-        output += ' ';
+        output += (op.size() == 1)? "" : " ";
         op.pop();
     }
-    return ValidString(output);
+    return output;
 }
 
 
 int BasicCalculator(int num1, int num2, char op) {
-    int res = 0;
     switch (op) {
     case '^':
-        res = pow(num2, num1);
-        break;
+        return  pow(num2, num1);
     case '*':
-        res = num2 * num1;
-        break;
+        return num2 * num1;
     case '/':
-        res = num2 / num1;
-        break;
+        return num2 / num1;
     case '+':
-        res = num2 + num1;
-        break;
+        return num2 + num1;
     case '-':
-        res = num2 - num1;
-        break;
+        return num2 - num1;
     }
-    return res;
 }
 
 
@@ -131,16 +125,16 @@ int PostfixCalculator(string postfix) {
     bool check = false;
     for (int i = 0; i < postfix.length();i++) {
         if (!IsOperator(postfix[i])) {
-            
+
             number = number * 10 + postfix[i] - '0';
             check = true;
         }
-        else if (number > 0|| check) {
+        else if (number > 0 || check) {
             output.push(number);
             number = 0;
             check = false;
         }
-        else if(postfix[i] != ' ')
+        else if (postfix[i] != ' ')
         {
             int num1 = output.top();
             output.pop();
@@ -158,15 +152,5 @@ int main()
     string s;
     cin >> s;
     cout << PostfixCalculator(ConvertInfixToPostfix(s)) << endl;
+    
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
--- comment.py
+++
@@ -1,6 +0,0 @@
-#ghi chú
-# in ra màn hình dòng chữ hello world
-print("hello world")
-
-# chuỗi có dạng là một comment
-print("# đây là một comment")
